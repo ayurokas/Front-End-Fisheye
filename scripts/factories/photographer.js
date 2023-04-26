c1c28e1904499d3c8aaf231445659a96eb0d7410
perso <elisa.github@gmail.com>
2023-04-27 00:21:36 +0200

    update

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,37 @@
+//fonction pour récupérer et afficher les éléments des photographes sur la page photographe
+// eslint-disable-next-line no-unused-vars
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, id, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const article = document.createElement('article');
+        const lien = document.createElement('a');
+        lien.setAttribute("href", "photographer.html?id=" + id)
+        const img = document.createElement('img');
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", name);
+        const h2 = document.createElement('h2');
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        const h3 = document.createElement('h3');
+        h3.textContent = `${city}, ${country}`;
+        const slogan = document.createElement('p');
+        slogan.textContent = tagline;
+        slogan.classList.add("slogan");
+        const prix = document.createElement('p');
+        prix.textContent = price + " €/jour";
+        prix.classList.add("prix");
+        lien.appendChild(img);
+        lien.appendChild(h2);
+        article.appendChild(lien);
+        article.appendChild(h3);
+        article.appendChild(slogan);
+        article.appendChild(prix);
         return (article);
     }
     return { name, picture, getUserCardDOM }
+
 }
+
+
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,46 +1,48 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
+async function getPhotographers() {
+    const response = await fetch('https://raw.githubusercontent.com/ayurokas/Front-End-Fisheye/main/data/photographers.json');
+    const data = await response.json();
+    console.log(data); // Affiche les données renvoyées par l'API dans la console
+    return data;
+}
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+async function displayData(data) {
+    const photographers = data.photographers;
+    const photographersSection = document.querySelector(".photographer_section");
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    };
+    photographers.forEach((photographer) => {
+        const photographerModel = photographerFactory(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    });
+};
+// Ajoute un gestionnaire d'événement click à chaque photographe
+const photographers = document.querySelectorAll('.photographer');
+photographers.forEach((photographer) => {
+    photographer.addEventListener('click', (event) => {
+        const id = photographer.dataset.id;
+        changePageUrl(id);
+    });
+});
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
-    
-    init();
-    
+// Fonction pour changer l'URL de la page
+function changePageUrl(id) {
+    const url = new URL(window.location.href);
+    url.searchParams.set('id', id);
+    history.pushState({}, '', url.toString());
+}
+
+// Fonction pour charger les données du photographe correspondant à l'id
+async function loadPhotographer(id) {
+    const response = await fetch(`https://raw.githubusercontent.com/ayurokas/Front-End-Fisheye/main/data/photographers.json`);
+    const data = await response.json();
+    const photographer = data.photographers.find(p => p.id == id);
+    // Afficher les données du photographe sur la page
+}
+
+
+async function init() {
+    const data = await getPhotographers();
+    displayData(data);
+};
+
+init();
