237f5ff31aee096f8de6f6e423fd061f18337042
perso <elisa.github@gmail.com>
2023-04-28 14:33:01 +0200

    update

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,48 +1,37 @@
+// Code pour la page d'accueil
+
 async function getPhotographers() {
     const response = await fetch('https://raw.githubusercontent.com/ayurokas/Front-End-Fisheye/main/data/photographers.json');
     const data = await response.json();
     console.log(data); // Affiche les données renvoyées par l'API dans la console
     return data;
-}
-
-async function displayData(data) {
+  }
+  
+  function displayPhotographers(data) {
     const photographers = data.photographers;
-    const photographersSection = document.querySelector(".photographer_section");
-
+    const photographersSection = document.querySelector('.photographer_section');
+  
     photographers.forEach((photographer) => {
-        const photographerModel = photographerFactory(photographer);
-        const userCardDOM = photographerModel.getUserCardDOM();
-        photographersSection.appendChild(userCardDOM);
+      const photographerModel = photographerFactory(photographer);
+      const userCardDOM = photographerModel.getUserCardDOM();
+      const photographerLink = userCardDOM.querySelector('a'); // Récupérer le lien dans la carte de photographe
+      photographerLink.addEventListener('click', (event) => {
+        event.preventDefault(); // Empêcher le comportement par défaut de la balise "a"
+        const photographerId = photographer.id;
+        window.location.href = `photographer.html?id=${photographerId}`; // Rediriger l'utilisateur vers la page photographe avec l'id du photographe comme paramètre de l'url
+      });
+      photographerLink.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+          event.preventDefault(); // Empêcher le comportement par défaut de la balise "a"
+          const photographerId = photographer.id;
+          window.location.href = `photographer.html?id=${photographerId}`; // Rediriger l'utilisateur vers la page photographe avec l'id du photographe comme paramètre de l'url
+        }
+      });
+      photographersSection.appendChild(userCardDOM);
     });
-};
-// Ajoute un gestionnaire d'événement click à chaque photographe
-const photographers = document.querySelectorAll('.photographer');
-photographers.forEach((photographer) => {
-    photographer.addEventListener('click', (event) => {
-        const id = photographer.dataset.id;
-        changePageUrl(id);
-    });
-});
-
-// Fonction pour changer l'URL de la page
-function changePageUrl(id) {
-    const url = new URL(window.location.href);
-    url.searchParams.set('id', id);
-    history.pushState({}, '', url.toString());
-}
-
-// Fonction pour charger les données du photographe correspondant à l'id
-async function loadPhotographer(id) {
-    const response = await fetch(`https://raw.githubusercontent.com/ayurokas/Front-End-Fisheye/main/data/photographers.json`);
-    const data = await response.json();
-    const photographer = data.photographers.find(p => p.id == id);
-    // Afficher les données du photographe sur la page
-}
-
-
-async function init() {
+  }
+  
+  async function init() {
     const data = await getPhotographers();
-    displayData(data);
-};
-
-init();
+    displayPhotographers(data);
+  }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,26 @@
-//Mettre le code JavaScript lié à la page photographer.html
+// Code pour la page photographe
+
+async function getPhotographerMedia() {
+    const response = await fetch('https://raw.githubusercontent.com/ayurokas/Front-End-Fisheye/main/data/photographers.json');
+    const data = await response.json();
+    console.log(data); // Affiche les données renvoyées par l'API dans la console
+    return data;
+  }
+  
+  function displayPhotographerInfo(data, photographerId) {
+    const photographer = data.photographers.find((p) => p.id == photographerId);
+    const photographerName = document.querySelector('h1');
+    if (photographerName !== null) {
+      photographerName.innerText = photographer.name;
+    }
+  }
+  
+  
+  async function init() {
+    const data = await getPhotographerMedia();
+    const urlParams = new URLSearchParams(window.location.search);
+    const photographerId = urlParams.get('id');
+    displayPhotographerInfo(data, photographerId);
+  }
+  
+  init();
